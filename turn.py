469a7af3773578b7b6d16a0f995ce81cd4bf14a1
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-06-11 18:20:43 -0400

    kerbal factor baisicly there

--- turn.py
+++ turn.py
@@ -28,25 +28,41 @@ def speed(speed):
     pair.set_default_speed(speed)
 
 def turn(deg, kerbal_factor = 3):
-    t.reset()
     m.reset_yaw_angle()
     pair.start_tank(30,-30)
-    while m.get_yaw_angle() <= deg - 10:
+    while m.get_yaw_angle() <= deg - 5:
         y = m.get_yaw_angle()
         print("turn is now at:{}".format(y))
     go_stop()
+    wait_for_seconds(0.1)
     y = m.get_yaw_angle()
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 1 success: {}".format(y))
+        return 
     print("Phase 1 complete...   turn at {}.".format(y))
-    pair.start_tank(-15, 15)
+    pair.start_tank(-10, 10)
     while m.get_yaw_angle() >= deg:
         y = m.get_yaw_angle()
         print("turn_is_now_at:{}".format(y))
-    pair.start_tank(7, -7)
+    go_stop()
+    wait_for_seconds(0.1)
+    y = m.get_yaw_angle()
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 2 success: {}".format(y))
+        return
+    print("Phase 2 complete...turn at {}.".format(y))
+    pair.start_tank(5, -5)
     while m.get_yaw_angle() <= deg:
         y = m.get_yaw_angle()
         print("turn is now at:{}".format(y))
     y = m.get_yaw_angle()
-    print("Done - turn ended at {}*.took {} seconds. we are off by {} degress".format(y, t.now(), y - deg))
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 2 success: {}".format(y))
+        return
+    print("Phase 2 complete...turn at {}.".format(y))
+    print("Done - turn ended at {}*. we are off by {} degress".format(y, y - deg))
 
-turn(100, 3)
+t.reset()
+turn(100, 4)
+print("took {} seconds".format(t.now()))
 go_stop()
