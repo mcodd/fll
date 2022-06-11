3aa6669a151d7179c2faca14af0864c337373693
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-06-11 17:43:51 -0400

    the start of the kerbal

--- turn.py
+++ turn.py
@@ -27,23 +27,26 @@ def go_stop():
 def speed(speed):
     pair.set_default_speed(speed)
 
-def turn(deg):
+def turn(deg, kerbal_factor = 3):
     t.reset()
     m.reset_yaw_angle()
     pair.start_tank(30,-30)
-    while m.get_yaw_angle() <= deg:
+    while m.get_yaw_angle() <= deg - 10:
         y = m.get_yaw_angle()
         print("turn is now at:{}".format(y))
+    go_stop()
+    y = m.get_yaw_angle()
+    print("Phase 1 complete...   turn at {}.".format(y))
     pair.start_tank(-15, 15)
-    while m.get_yaw_angle() => deg:
+    while m.get_yaw_angle() >= deg:
         y = m.get_yaw_angle()
         print("turn_is_now_at:{}".format(y))
-    pair.start_tank(7.5, -7.5)
+    pair.start_tank(7, -7)
     while m.get_yaw_angle() <= deg:
         y = m.get_yaw_angle()
         print("turn is now at:{}".format(y))
     y = m.get_yaw_angle()
-    print("Done - turn ended at {}*.took {} seconds. we are off by {} degress".format(y, t.now(),y - deg))
+    print("Done - turn ended at {}*.took {} seconds. we are off by {} degress".format(y, t.now(), y - deg))
 
-turn(90)
+turn(100, 3)
 go_stop()
