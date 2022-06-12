2ccc903c99f45d12d962cb53058f8a605f0562e2
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-06-12 10:46:14 -0400

    start of debug factor

--- turn.py
+++ turn.py
@@ -2,15 +2,6 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-hub = PrimeHub()
-right = Motor('A')
-left = Motor('B')
-sound = App()
-
-t = Timer()
-m = hub.motion_sensor
-pair = MotorPair('A', 'B')
-
 def go_forward():
     pair.set_default_speed(100)
     pair.start()
@@ -19,31 +10,55 @@ def go_backward():
     pair.set_default_speed(-100)
     pair.start()
 
-def go_stop():
+def pair_stop():
     pair.stop()
-    right.stop()
-    left.stop()
 
 def speed(speed):
     pair.set_default_speed(speed)
 
-def turn(deg):
-    t.reset()
+def turn(deg, kerbal_factor = 3):
     m.reset_yaw_angle()
     pair.start_tank(30,-30)
-    while m.get_yaw_angle() <= deg:
+    while m.get_yaw_angle() <= deg - 5:
         y = m.get_yaw_angle()
         print("turn is now at:{}".format(y))
-    pair.start_tank(-15, 15)
-    while m.get_yaw_angle() => deg:
-        y = m.get_yaw_angle()
-        print("turn_is_now_at:{}".format(y))
-    pair.start_tank(7.5, -7.5)
-    while m.get_yaw_angle() <= deg:
-        y = m.get_yaw_angle()
-        print("turn is now at:{}".format(y))
+    go_stop()
+    wait_for_seconds(0.1)
     y = m.get_yaw_angle()
-    print("Done - turn ended at {}*.took {} seconds. we are off by {} degress".format(y, t.now(),y - deg))
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 1 success: {}".format(y))
+        return
+    print("Phase 1 failed...trun at {}.".format(y))
+    pair.start_tank(-10, 10)
+    while m.get_yaw_angle() >= deg:
+        y = m.get_yaw_angle()
+        print("turn at:{}".format(y))
+    go_stop()
+    wait_for_seconds(0.1)
+    y = m.get_yaw_angle()
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 2 success: {}".format(y))
+        return
+    print("Phase 2 failed...turn at {}.".format(y))
+    pair.start_tank(5, -5)
+    while m.get_yaw_angle() <= deg:
+        y = m.get_yaw_angle()
+        print("turn at:{}".format(y))
+    go_stop()
+    wait_for_seconds(0.1)
+    y = m.get_yaw_angle()
+    if y >= deg - kerbal_factor and y <= deg + kerbal_factor:
+        print("phase 3 failed: {}".format(y))
+        return
+    print("Phase 3 failed...turn at {}.".format(y))
+    print("Failure - turn ended at {}*. off by {} degress".format(y, y - deg))
 
-turn(90)
-go_stop()
+def debug(level, msg):
+    if DEBUG_FACTOR >= level:
+        print(msg)
+
+hub = PrimeHub()
+t = Timer()
+m = hub.motion_sensor
+DEBUG_FACTOR = 0
+# START YOUR CODE HERE - example at https://github.com/FIXME
